bcc7492ea13d843d7a17e95d2738c6012cb0cdc3
Marcel Wozniak <marwozniak01@gmail.com>
2025-10-27 15:18:03 +0100

    font optimization

--- printforge/app/layout.tsx
+++ printforge/app/layout.tsx
@@ -1,4 +1,17 @@
 import "./globals.css";
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates",
+});
 
 export default function RootLayout({
   children,
@@ -7,26 +20,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${albertSans.className} ${montserratAlternates.variable}`}
+      >
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
             <div className="relative">
-              {/* Desktop logo */}
+              {/* Desktop Logo */}
               <img
-                src="/printforge-logo.svg"
+                src={"/printforge-logo.svg"}
                 alt="PrintForge Logo"
                 className="w-[200px] h-auto hidden md:block"
               />
-              {/* Mobile logo */}
+              {/* Mobile Logo */}
               <img
-                src="/printforge-logo-icon.svg"
+                src={"/printforge-logo-icon.svg"}
                 alt="PrintForge Logo"
                 className="w-[40px] h-auto block md:hidden"
               />
             </div>
             <ul className="flex items-center gap-2.5">
               <p>3D Models</p>
-              <p>About</p>
+              <p href="/about">About</p>
             </ul>
           </nav>
         </header>
